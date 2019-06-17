08926c95fbd1da6d14332e9b58b7bae53b343e4f
Skillvendor <skillvendor193@gmail.com>
2019-06-17 19:11:11 +0300

    A vannila node.js server that writes to a file

---
+++ 1-assignment-1/routes.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  res.setHeader('Content-Type', 'text/html');
+
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>GREETINGS FROM INDEX</title></head>');
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/users') {
+    res.write('<html>');
+    res.write('<head><title>Display Users</title></head>');
+    res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/create-user' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      console.log(message)
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
+    });
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+};
+
+// module.exports = requestHandler;
+
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some text';
+
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text';
